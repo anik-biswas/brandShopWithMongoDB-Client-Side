8a456d93386462c216073961444565982ebf760b
anik-biswas <anik.biswas.an@gmail.com>
2023-10-20 12:37:52 -0700

    Cart Delete fuction added

--- src/component/cartList/CartList.jsx
+++ src/component/cartList/CartList.jsx
@@ -1,22 +1,77 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../firebase/AuthProvider";
+import Swal from "sweetalert2";
 
 const CartList = () => {
     const location = useLocation();
     const navigate = useNavigate();
-    const carts = useLoaderData() ;
+   // const carts = useLoaderData() ;
     const{user}= useContext(AuthContext);
+    
    // console.log(cart);
     // const { email, name, bName,price,rating,category,description,pImage }= cart;
     
     const email = user.email;
    // console.log(email)
+            const [carts, setCarts] = useState([]);
+            const [filterCarts, setFilteredCart] = useState([]);
+                
+            useEffect(() => {
+                fetch('http://localhost:5000/cart')
+                    .then((res) => res.json())
+                    .then((data) => {
+                        setCarts(data);
+                        filteredCarts(data, email);
+                    });
+            }, [email]);
+            // const filteredCarts = carts.filter((cart) => {
+            // return cart.email === email;
+            // });
+            const filteredCarts= (carts, email) => {
+                const filtered = carts.filter((cart) => cart.email === email);
+                setFilteredCart(filtered);
+            };
 
-            const filteredCarts = carts.filter((cart) => {
-            return cart.email === email;
-            });
-            console.log(filteredCarts);
+            console.log(filterCarts);
+
+            // const [updateCart, setUpdateCart] = useState(filteredCarts);
+
+            const handleDelete = id => {
+                console.log(id)
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "Delete Product won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+        
+                        fetch(`http://localhost:5000/cart/${id}`, {
+                            method: 'DELETE'
+                        })
+                            .then(res => res.json())
+                            .then(data => {
+                                console.log(data);
+                                if (data.deletedCount > 0) {
+                                    Swal.fire(
+                                        'Deleted!',
+                                        'Your cart has been deleted.',
+                                        'success'
+                                    )
+                                    const remainingCart = filterCarts.filter(cart => cart._id !== id);
+                                    setFilteredCart(remainingCart);
+                            
+                                }
+                            })
+        
+                    }
+                })
+            }
+
     return (
         <div>
             <div>
@@ -37,7 +92,7 @@ const CartList = () => {
                     </thead>
                     <tbody>
                         {
-                            filteredCarts.map((cart, index) => <tr key={user._id}>
+                            filterCarts.map((cart, index) => <tr key={user._id}>
                                 <th>{index+1}</th>
                                 <th>{cart.name}</th>
                                 <th><img src={cart.pImage}  className="w-16 h-16" alt="" /></th>
@@ -45,10 +100,11 @@ const CartList = () => {
                                 <td>{cart.category}</td>
                                 <td>{cart.price}</td>
                                 <td>
-                                    <button
+                                    <button onClick={() => handleDelete(cart._id)}
                                          className="btn">X</button>
                                 </td>
                             </tr>)
+                           
                         }
 
                     </tbody>
